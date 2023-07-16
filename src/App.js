914fee98494e28f7e994e8522697b3f913b69f74
Cihan Eroğlu <ceroglu7@gmail.com>
2023-07-16 18:15:01 +0300

    Useref Başlık Değiştirici

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useRef } from "react"
+
+const App = ()=>{
+    const eleman1=useRef()
+    const baslikGuncelle=()=>{
+        document.title=eleman1.current.value
+    }
+    return(
+        <>
+        <input type="text" ref={eleman1}></input>
+        <button onClick={baslikGuncelle}>Başlık Güncelle</button>
+        </>
+    )
+}
+
+export default App
